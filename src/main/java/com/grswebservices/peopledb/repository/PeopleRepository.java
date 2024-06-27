366c1a0dbf22f9bc1c1fd16d90419fbb680ccb77
grs-se <george_ricesmith@hotmail.com>
2024-06-27 11:59:13 +0100

    Initial commit. Create new DB project

---
+++ src/main/java/com/grswebservices/peopledb/repository/PeopleRepository.java
@@ -0,0 +1,10 @@
+package com.grswebservices.peopledb.repository;
+
+import com.grswebservices.peopledb.model.Person;
+
+public class PeopleRepository {
+
+    public Person save(Person person) {
+        return person;
+    }
+}
